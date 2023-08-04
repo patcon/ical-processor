c78b2f365750104db286555bab1f0bd4fa2f2d6a
Patrick Connolly <patrick.c.connolly@gmail.com>
2023-08-04 19:14:29 +0100

    Made preview and download buttons work, and form field reactive.

--- static/app.js
+++ static/app.js
@@ -1,13 +1,23 @@
 const app = Vue.createApp({
   data() {
       return {
-          message: "banana"
+          icalUrl: "https://calendar.google.com/calendar/ical/8ba1e5d2f51d2872501c5a28473e8e954c0cae2471468db75ba740bc1abc8036%40group.calendar.google.com/public/basic.ics",
+          trackedUrl: "",
+          trackedUrlPreview: "",
       }
   },
+  mounted() {
+    this.initialize();
+  },
   methods: {
-      testMethod() {
-          return this.message + "!";
-      }
+      handleIcalUrlInput(event) {
+        this.icalUrl = event.target.value;
+        this.trackedUrl = `http://127.0.0.1:5000/parse-ical?ical_url=${this.icalUrl}`;
+        this.trackedUrlPreview = `http://127.0.0.1:5000/parse-ical?debug=1&ical_url=${this.icalUrl}`;
+      },
+      initialize() {
+        this.trackedUrl = `http://127.0.0.1:5000/parse-ical?ical_url=${this.icalUrl}`;
+      },
   }
 })
 // Delimiters changed to ES6 template string style
